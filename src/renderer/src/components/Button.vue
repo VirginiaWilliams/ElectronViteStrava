7e59790ec02cefb95185fa393435609b6fd93794
VirginiaWilliams <ginnyglink@gmail.com>
2025-09-14 14:23:47 -0700

    Basic setup without strava stuff

---
+++ src/renderer/src/components/Button.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="button">{{ props.text }}</div>
+</template>
+
+<style lang="css">
+.button {
+  flex-shrink: 0;
+  margin: 6px;
+  cursor: pointer;
+  text-decoration: none;
+  display: inline-block;
+  border: 1px solid transparent;
+  text-align: center;
+  font-weight: 600;
+  white-space: nowrap;
+  border-radius: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  font-size: 14px;
+  border-color: var(--ev-button-alt-border);
+  color: var(--ev-button-alt-text);
+  background-color: var(--ev-button-alt-bg);
+}
+
+.button:hover {
+  border-color: var(--ev-button-alt-hover-border);
+  color: var(--ev-button-alt-hover-text);
+  background-color: var(--ev-button-alt-hover-bg);
+}
+</style>
---
+++ src/renderer/src/components/TopBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="topbar">{{ props.text }}</div>
+</template>
+
+<style lang="css">
+@import '../assets/base.css';
+
+.topbar {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 3rem;
+  background-color: var(--ev-c-gray-1);
+}
+</style>
